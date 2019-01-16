d305fadf7830cfb874fbd13bce89d9c67c668c6f
Aersum <antooniosivanovs@gmail.com>
2019-01-16 04:57:29 +0200

    Add Profile and Hobbie Moddels, add it to admin Panel

--- members/models.py
+++ members/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    image = models.ImageField(upload_to='profile_image', blank=True)
+
+
+class Hobbie(models.Model):
+    title = models.CharField(max_length=100, null=True, blank=True)
+    author = models.ManyToManyField(Profile, blank=True)
