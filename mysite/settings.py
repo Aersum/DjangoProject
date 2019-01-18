0e17c7cbef7d28883c7cc53a54d564c79a99a756
Aersum <antoniosivanovs@gmail.com>
2019-01-18 15:48:38 +0000

    Fixing bugs with font awesome and menu collapsing

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,4 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'members_static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),'static')
