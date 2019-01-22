5117aa1b4cc40cfc3f0002ad3adcf01cf25ec436
Aersum <antooniosivanovs@gmail.com>
2019-01-22 04:49:26 +0200

    Add Detailview for events. Fix links for home and event detail view

--- members/urls.py
+++ members/urls.py
@@ -4,4 +4,5 @@ from members import views
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
     path("hello/<name>", views.hello_there, name="hello_there"),
+    path("event/<int:pk>", views.EventDetailView.as_view(), name="event"),
 ]
