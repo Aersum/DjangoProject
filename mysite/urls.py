a29ddccf65840173998a65a5d5e66df7ecd16b88
Aersum <antoniosivanovs@gmail.com>
2019-01-22 08:26:52 +0000

    Fix bug with url reversing on nginx

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("members.urls"))
+    path("", include(('members.urls', 'members'), namespace='members'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
