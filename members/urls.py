d2e4c89176abb0da39e3f487ca2813555d5912bb
Aersum <antooniosivanovs@gmail.com>
2019-01-30 01:44:33 +0200

    Add user registration that expand standart UserRegistrationForm. Add profile view

--- members/urls.py
+++ members/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("event/<int:pk>", views.EventDetailView.as_view(), name="event"),
     path('login/', views.LoginFormView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
-    path("hello/<name>", views.hello_there, name="hello_there"),
+    path('register/', views.RegisterFormView.as_view(), name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('hello/<name>', views.hello_there, name='hello_there'),
 ]
--- members/views.py
+++ members/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse, HttpResponseRedirect
 from datetime import datetime
 from django.views.generic import ListView, DetailView, FormView, View
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
 from django.contrib.auth import login, logout
 from django.contrib import messages
 from .models import Event
+from members.forms import RegistrationForm
 
 
 class IndexView(ListView):
@@ -39,6 +41,33 @@ class LogoutView(View):
     def get(self, request):
         logout(request)
         return HttpResponseRedirect('/')
+
+
+class RegisterFormView(FormView):
+    form_class = RegistrationForm
+    template_name = 'members/register.html'
+    success_url = '/login/'
+
+    def form_valid(self, form):
+        form.save()
+        return super(RegisterFormView, self).form_valid(form)
+
+    def form_invalid(self, form):
+        return super(RegisterFormView, self).form_invalid(form)
+
+
+class ProfileDetailView(DetailView):
+    model = User
+    template_name = 'members/profile.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProfileDetailView, self).get_context_data(**kwargs)
+        context['user'] = self.request.user
+
+
+def profile(request):
+    args = {'user': request.user}
+    return render(request, 'members/profile.html', args)
 
 
 def hello_there(request, name):
