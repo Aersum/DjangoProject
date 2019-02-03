3096a8f91f9a270322e8f26a0cf5a7e1569d4f12
Aersum <antooniosivanovs@gmail.com>
2019-02-03 14:18:22 +0200

    Add automatic blanck profile creation when creating an new user. Add UserChangeForm to edit user info

--- members/models.py
+++ members/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.db.models.signals import post_save
 
 
 class Hobby(models.Model):
@@ -13,13 +14,23 @@ class Hobby(models.Model):
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    city = models.CharField(max_length=100, null=True, blank=True)
+    city = models.CharField(max_length=100, null=True, blank=True, default='')
     birth_date = models.DateField(null=True, blank=True)
     image = models.ImageField(upload_to='images/profile_img', blank=True)
-    hobby = models.ManyToManyField(Hobby, blank=True)
+    hobby = models.ManyToManyField(Hobby, blank=True, default='')
 
     def __str__(self):
         return self.user.username
+
+
+def create_profile(sender, **kwargs):
+    user = kwargs["instance"]
+    if kwargs["created"]:
+        user_profile = Profile(user=user)
+        user_profile.save()
+
+
+post_save.connect(create_profile, sender=User)
 
 
 class Event(models.Model):
--- members/urls.py
+++ members/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from members import views
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("event/<int:pk>", views.EventDetailView.as_view(), name="event"),
+    path('', views.IndexView.as_view(), name='index'),
+    path('event/<int:pk>', views.EventDetailView.as_view(), name='event'),
     path('login/', views.LoginFormView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('register/', views.RegisterFormView.as_view(), name='register'),
-    path('profile/', views.profile, name='profile'),
+    path('profile/', views.view_profile, name='profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('hello/<name>', views.hello_there, name='hello_there'),
 ]
